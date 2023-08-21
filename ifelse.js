6e2e60d8f15ccb47994ce197f1d0c5cbe2d7a5b6
NandhiniJayaraman <nansjayaraman@gmail.com>
2023-08-21 10:31:47 +0530

    if else first stage

---
+++ ifelse.js
@@ -0,0 +1,32 @@
+// Even / odd
+a=23
+if(a%2==0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+
+// Larger two number
+a=24
+b=78
+if(a>b){
+    console.log(a,"is a larger number")
+}
+else{
+    console.log(b,"is a larger number")
+}
+// smallest number
+a=34
+b=45
+c=12
+if((a>b)&&(b<c)){
+    console.log(b,"is a smallest number")
+}
+else if((a>c)&&(b>c)){
+    console.log(c,"is a smallest number")
+}
+else{
+    console.log(a,"is smallest number")
+}
+// 
